a738e5b5dbf5853e7911efad9ff5cc0da77c9788
MrEldar <mr_eldar_achlo@mail.ru>
2024-12-31 13:45:26 +0300

    тесты в js файле

--- Preview_pages/page__kezenoi-am/js/maps.js
+++ Preview_pages/page__kezenoi-am/js/maps.js
@@ -14,7 +14,7 @@ function init() {
                 <div class="balloon-content">
                     <h3>Озеро Кезеной-Ам</h3>
                     <p>Высокогорное озеро Кезенойам — самое большое по площади озеро Чечни и Большого Кавказа.</p>
-                    <img src="/Images/1.jpg" alt="Кезеной-Ам">
+                    <img src="images/1.jpg" alt="Кезеной-Ам">
                 </div>`,
 			},
 			{
